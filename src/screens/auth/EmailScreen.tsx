693a70129fdb2b2efa4d1b500bc5c13be065557c
Wasim <ansitwasim297@gmail.com>
2024-12-20 16:44:03 +0530

    design:-main-screen for login

--- src/screens/auth/EmailScreen.tsx
+++ src/screens/auth/EmailScreen.tsx
@@ -1,41 +1,129 @@
-import { View, Text, Button, TextInput, StyleSheet } from 'react-native'
+import { View, Text, Button, TextInput, StyleSheet, Image, TouchableOpacity, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { AuthStackParamList } from '../../navigator/AuthNavigator';
-
+import Colors from '../../constants/Colors';
 
 type Props = NativeStackScreenProps<AuthStackParamList, 'EmailScreen'>;
-const EmailScreen :React.FC<Props>=({navigation})=> {
+ const EmailScreen :React.FC<Props>=({navigation})=> {
     const [email,setEmail]=useState('')
-    const onContinue=()=>{
+    const handleContinue=()=>{
         navigation.navigate('PasswordScreen',{email:email})
     }
-  return (
-    <View>
-      <Text>EmailScreen</Text>
-      <TextInput
-         value={email}
-         onChangeText={setEmail}
-          style={styles.input}
-          placeholder="Enter your E-mail."
-          placeholderTextColor="gray"
-          keyboardType="email-address"
-          autoCapitalize="none"
+    const handleBack = () =>{};
+  
+    return (
+      <View style={styles.mainScreen}>
+        {/* Logo Section */}
+        <View style={styles.upperPart}>
+          <Image
+             source={require('../../assets/Images/houselogo1.png')} 
+              style={styles.image}
+            resizeMode="contain"
           />
-      <Button onPress={onContinue} title="Continue" />
-      <Button onPress={navigation.goBack} title="Back" />
-    </View>
-  )
-}
-const styles =StyleSheet.create({
-    input: {
-        minHeight: 50,
-        borderColor: 'gray',
-        borderWidth: 1,
-        marginBottom: 20,
-        paddingHorizontal: 10,
-        borderRadius: 15,
-      },
-})
+        </View>
+        {/* Input Section / Lower Part */}
+        
+        <View style={[styles.lowerPart]}>
+        <View style={styles.txtpadding}>
+          <Text style={[styles.label]}>Email or Mobile</Text>
+          <TextInput
+            placeholder="Email or Mobile"
+            value={email}
+            onChangeText={setEmail}
+            style={[styles.input,styles.spacing1]}
+          />
+        </View>
+  
+        {/* Buttons Section */}
+        <View style={styles.btnsection}>
+          <TouchableOpacity style={[styles.button, styles.spacing]}  onPress={handleContinue}>
+            <Text style={styles.buttonText}
+            >Continue</Text>
+          </TouchableOpacity>
 
-export default EmailScreen;
+          <TouchableOpacity style={[styles.button, styles.spacing,styles.color]}  onPress={navigation.goBack}>
+            <Text style={styles.buttonText}>Back</Text>
+          </TouchableOpacity>
+        </View>
+        </View>
+      </View>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    mainScreen:{
+      flex:1,
+      backgroundColor: '#cc0e74', // Pinkish Background
+    },
+    color:{
+      backgroundColor: '#790c5a',
+    },
+    txtpadding:{
+    paddingLeft:10,
+    width:'95%',
+    },
+  btnsection:{
+  justifyContent: 'center', // Centers vertically
+  alignItems: 'center', 
+
+    },
+    upperPart:{
+      flex: 2,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#cc0e74', // Same as main background
+      borderBottomRightRadius: 60,
+    },
+    lowerPart:{
+      flex: 3,
+      backgroundColor: '#ffffff', // White background for lower part
+      borderTopLeftRadius: 70,
+      paddingVertical: 60,
+      
+    },
+    image:{
+      width: '70%',
+      height: '30%',
+    },
+    label: {
+      fontSize: 16,
+      color: '#880e4f', // Dark pink
+      marginBottom: 10,
+      fontWeight: 'bold',
+    },
+    input: {
+      borderBottomWidth: 1,
+      borderBottomColor: '#880e4f',
+      padding: 5,
+      fontSize: 16,
+    },
+    button:{
+      backgroundColor: '#cc0e74', // Matching pink button
+      padding: 15,
+      borderRadius: 30,
+      marginVertical: 10,
+      width: '95%',
+      alignItems: 'center',
+      justifyContent:'center',
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 5,
+    },
+    spacing:{
+      marginBottom: 10, // Adds space below each button
+    },
+    spacing1:{
+      marginBottom: 45, // Adds space below each button
+    },
+    buttonText:{
+      color: '#ffffff',
+      fontSize: 16,
+      fontWeight: 'bold',
+
+    },
+  });
+
+  export default EmailScreen;
--- src/screens/auth/PasswordScreen.tsx
+++ src/screens/auth/PasswordScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Button, TextInput, StyleSheet } from 'react-native'
+import { View, Text, Button, TextInput, StyleSheet, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { AuthStackParamList } from '../../navigator/AuthNavigator';
@@ -8,30 +8,130 @@ type Props = NativeStackScreenProps<AuthStackParamList, 'PasswordScreen'>;
 
 const PasswordScreen :React.FC<Props>=({navigation})=> {
 const onContinue =()=>{}
-  return (
-      <View>
-      <Text>Password Screen</Text>
-      <TextInput
-          style={styles.input}
-          placeholder="Enter your E-mail."
-          placeholderTextColor="gray"
-          keyboardType="email-address"
-          autoCapitalize="none"
-          />
-      <Button onPress={onContinue} title="Continue" />
-      <Button onPress={navigation.goBack} title="Back" />
+const handleContinue=() =>{}
+return (
+  <View style={styles.mainScreen}>
+    {/* Logo Section */}
+    <View style={styles.upperPart}>
+      <Image
+         source={require('../../assets/Images/houselogo1.png')} 
+          style={styles.image}
+        resizeMode="contain"
+      />
     </View>
-  )
-}
-const styles =StyleSheet.create({
-    input: {
-        minHeight: 50,
-        borderColor: 'gray',
-        borderWidth: 1,
-        marginBottom: 20,
-        paddingHorizontal: 10,
-        borderRadius: 15,
-      },
-})
+    {/* Input Section / Lower Part */}
+    
+    <View style={[styles.lowerPart]}>
+    <View style={styles.txtpadding}>
+      <Text style={[styles.label]}>Password</Text>
+      <TextInput
+        placeholder="Password"
+        style={[styles.input,styles.spacing1]}
+      />
+    </View>
+    <TouchableOpacity style={styles.forgotPassword}>
+          <Text style={styles.forgotText}>Forget Password?</Text>
+        </TouchableOpacity>
+
+    {/* Buttons Section */}
+    <View style={styles.btnsection}>
+      <TouchableOpacity style={[styles.button, styles.spacing]}  onPress={handleContinue}>
+        <Text style={styles.buttonText}
+        >Sign In</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={[styles.button, styles.spacing,styles.color]}  onPress={navigation.goBack}>
+        <Text style={styles.buttonText}>Back</Text>
+      </TouchableOpacity>
+    </View>
+    </View>
+  </View>
+);
+};
+
+const styles = StyleSheet.create({
+mainScreen:{
+  flex:1,
+  backgroundColor: '#cc0e74', // Pinkish Background
+},
+forgotPassword:{
+  position: 'absolute',
+  right: '5%',
+  top: 135,
+  fontSize: 20,
+},
+forgotText:{
+  fontSize: 16,
+  color: '#cc0e74'
+
+},
+txtpadding:{
+paddingLeft:10,
+width:'95%',
+},
+btnsection:{
+justifyContent: 'center', // Centers vertically
+alignItems: 'center', 
+
+},
+upperPart:{
+  flex: 2,
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: '#cc0e74', // Same as main background
+  borderBottomRightRadius: 60,
+},
+lowerPart:{
+  flex: 3,
+  backgroundColor: '#ffffff', // White background for lower part
+  borderTopLeftRadius: 70,
+  paddingVertical: 60,
+  
+},
+image:{
+  width: '70%',
+  height: '30%',
+},
+label: {
+  fontSize: 20,
+  color: '#880e4f', // Dark pink
+  marginBottom: 10,
+  fontWeight: 'bold',
+},
+input: {
+  borderBottomWidth: 1,
+  borderBottomColor: '#880e4f',
+  padding: 5,
+  fontSize: 16,
+},
+button:{
+  backgroundColor: '#cc0e74', // Matching pink button
+  padding: 15,
+  borderRadius: 30,
+  marginVertical: 10,
+  width: '95%',
+  alignItems: 'center',
+  justifyContent:'center',
+  shadowColor: '#000',
+  shadowOffset: { width: 0, height: 2 },
+  shadowOpacity: 0.25,
+  shadowRadius: 3.84,
+  elevation: 5,
+},
+spacing:{
+  marginBottom: 10, // Adds space below each button
+},
+spacing1:{
+  marginBottom: 55, // Adds space below each button
+},
+buttonText:{
+  color: '#ffffff',
+  fontSize: 17,
+  fontWeight: 'bold',
+},
+color:{
+  backgroundColor: '#790c5a',
+},
+});
 
 export default PasswordScreen;
