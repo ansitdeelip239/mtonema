d8619ea91e14160aa07faf45a0a33f59a2f8f8d8
Deelip Kumar Singh <ansitdeelip239@gmail.com>
2025-06-06 10:41:32 +0530

    fix: Enable universal APK generation in build.gradle; update API URL configuration in index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,8 +3,8 @@ export const config = {
   // environment: 'production',
   hmacToken: 'e74e1523bfaf582757ca621fd6166361a1df604b3c6369383f313fba83baceac',
 
-  apiUrl: 'https://dncrnewapi-bmbfb6f6awd8b0bd.westindia-01.azurewebsites.net',
-  // apiUrl: 'https://mtestatesapi-f0bthnfwbtbxcecu.southindia-01.azurewebsites.net',
+  // apiUrl: 'https://dncrnewapi-bmbfb6f6awd8b0bd.westindia-01.azurewebsites.net',
+  apiUrl: 'https://mtestatesapi-f0bthnfwbtbxcecu.southindia-01.azurewebsites.net',
 };
 
 export default config;
