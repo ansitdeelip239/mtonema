43793ea846915b655f49bf5fc843af631efcfb7d
Deelip Kumar Singh <ansitdeelip239@gmail.com>
2025-07-31 11:50:27 +0530

    reafctor: Refactor iOS project structure and update Podfile
    - Changed target name from 'MTEstatesMA' to 'mtonema' in Podfile.
    - Created new Xcode project file for 'mtonema'.
    - Added Xcode scheme for 'mtonema' with build, test, launch, profile, analyze, and archive actions.
    - Implemented AppDelegate.swift for React Native integration.
    - Added AppIcon asset catalog with appropriate images and sizes.
    - Created Info.plist with necessary configurations for the app.
    - Developed LaunchScreen.storyboard for the app's launch screen.
    - Included PrivacyInfo.xcprivacy for privacy-related configurations.

--- ios/mtonema/AppDelegate.swift
+++ ios/mtonema/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "mtestates",
+      withModuleName: "mtonema",
       in: window,
       launchOptions: launchOptions
     )
